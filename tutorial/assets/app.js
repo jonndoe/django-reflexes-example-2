4ad7865ad9caf3b3c6f9704af86f696ad0215f78
jonndoe <lukichevvasily@gmail.com>
2021-06-28 19:19:40 +0100

    Added working simple webpack configuration

---
+++ tutorial/assets/app.js
@@ -0,0 +1,12 @@
+
+import "./css/bootstrap.lumen.min.css";
+import "./css/project.css";
+
+
+console.log("hello, world!");
+console.log("good good good");
+//document.getElementById("goodid").innerHTML = "Hello from Webpack! I'm refreshing automatically!";
+import("./js/bootstrap/bootstrap.bundle.min");
+import("./js/bootstrap/jquery.min");
+import("./js/bootstrap/prism");
+import("./js/project");
