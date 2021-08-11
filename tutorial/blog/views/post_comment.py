a8ce4e046aa992fe84b5f0c5b2d573c19e72738c
jonndoe <lukichevvasily@gmail.com>
2021-08-11 13:59:45 +0100

    Refactored templates

--- tutorial/blog/views/post_comment.py
+++ tutorial/blog/views/post_comment.py
@@ -2,7 +2,7 @@ from django.views.generic.base import TemplateView
 
 
 class PostCommentView(TemplateView):
-    template_name = 'post_comment.html'
+    template_name = 'blog/post_comment.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
